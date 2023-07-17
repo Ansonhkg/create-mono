e29b9ea49eaa59efe37234c636764f674054053e
Ansonhkg <ansonox@gmail.com>
2023-07-17 20:11:59 +0100

    fix: undefined package name

---
+++ packages/module-node/index.js
@@ -0,0 +1,3 @@
+// @ts-check
+
+export const hello = () => "Hello from module Node";
--- scripts/build.js
+++ scripts/build.js
@@ -8,7 +8,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 console.log("Reading root package.json...");
-const rootPackageJson = path.resolve(__dirname, "../package.json");
+const rootPackageJson = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "../package.json"), "utf8")
+);
 
 console.log("Resolving packages directory...");
 const packagesDir = path.join(__dirname, "../packages");
